c387196c564ceae743a3058999696fb2d46ec2fb
Nurlan Su <git@nurlan.co>
2017-05-10 19:52:39 +0200

    🔥  Added dark mode

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,3 +1,5 @@
+var btn = document.getElementById('btn')
+var checkbox = document.getElementById('switch')
 var fact = document.getElementById('fact')
 
 function randomFact() {
@@ -18,5 +20,17 @@ function randomFact() {
   }
 }
 
-var btn = document.getElementById('btn')
+function darkModeSwitch() {
+  if (checkbox.checked) {
+    document.body.classList.add('dark')
+  } else {
+    document.body.classList.remove('dark')
+  }
+  localStorage.setItem('switch', checkbox.checked)
+}
+
 btn.addEventListener('click', randomFact)
+checkbox.addEventListener('click', darkModeSwitch)
+
+checkbox.checked = localStorage.getItem('switch') === 'true' ? true : false
+darkModeSwitch()
