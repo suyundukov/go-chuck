6040ae23ce02bb08b92885076ba4352e7cdf5283
Nurlan Su <git@nurlan.co>
2019-03-07 19:01:04 +0700

    Added semicolons

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,37 +1,37 @@
-var btn = document.getElementById('btn')
-var checkbox = document.getElementById('switch')
-var fact = document.getElementById('fact')
+var btn = document.getElementById('btn');
+var checkbox = document.getElementById('switch');
+var fact = document.getElementById('fact');
 
 function randomFact() {
-  var req = new XMLHttpRequest()
-  var path = window.location.pathname
-  req.open('GET', '/api')
-  req.send()
+  var req = new XMLHttpRequest();
+  var path = window.location.pathname;
+  req.open('GET', '/api');
+  req.send();
   req.onload = function() {
-    res = JSON.parse(req.responseText)
-    resValue = res.value
-    chuckFact = resValue.fact
-    fact.innerHTML = chuckFact
-    url = resValue.id
-    permaLink.setAttribute('href', '/fact/' + url)
+    res = JSON.parse(req.responseText);
+    resValue = res.value;
+    chuckFact = resValue.fact;
+    fact.innerHTML = chuckFact;
+    url = resValue.id;
+    permaLink.setAttribute('href', '/fact/' + url);
   }
   // If user is not on the home page, return him back
   if (path != '/') {
-    history.replaceState('', 'Chuck Norris Facts', '/')
+    history.replaceState('', 'Chuck Norris Facts', '/');
   }
 }
 
 function darkModeSwitch() {
   if (checkbox.checked) {
-    document.body.classList.add('dark')
+    document.body.classList.add('dark');
   } else {
-    document.body.classList.remove('dark')
+    document.body.classList.remove('dark');
   }
-  localStorage.setItem('switch', checkbox.checked)
+  localStorage.setItem('switch', checkbox.checked);
 }
 
-btn.addEventListener('click', randomFact)
-checkbox.addEventListener('click', darkModeSwitch)
+btn.addEventListener('click', randomFact);
+checkbox.addEventListener('click', darkModeSwitch);
 
-checkbox.checked = localStorage.getItem('switch') === 'true' ? true : false
-darkModeSwitch()
+checkbox.checked = localStorage.getItem('switch') === 'true' ? true : false;
+darkModeSwitch();
